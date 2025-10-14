b64719b542a5432c3913dbf3221f3ab754266fbf
Lindsey <hey.ito@outlook.com>
2025-10-14 14:40:14 +0200

    Update start_jupyterlab_MODIFIED.bat

--- Ressourcen/start_jupyterlab_MODIFIED.bat
+++ Ressourcen/start_jupyterlab_MODIFIED.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001
-:: activate environment and launch Jupyter Lab
-cd "C:\Users\linca\DSCB130_Info-1"
+:: sub local path to user and location of miniconda3 folder
+cd "C:\Users\USER\DSCB130_Info-1"
 call C:\ProgramData\miniconda3\Scripts\activate.bat dscb130
 call jupyter lab
