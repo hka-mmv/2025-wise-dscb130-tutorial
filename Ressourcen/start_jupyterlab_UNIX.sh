70c8bf9d043e387a44ad7e9bf776dbb5c6a9195a
337ito <hey.ito@outlook.com>
2025-10-20 22:45:14 +0200

    add: more content juplab, bash script
    yay

---
+++ Ressourcen/start_jupyterlab_UNIX.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# dies ist ein Kommentar, und die Zeile über mir sagt dem System, 
+# dass dieses Skript mit bash ausgeführt werden soll :)
+
+# hier bitte den Pfad zu eurem gewünschten Arbeitsverzeichnis eintragen
+cd "/Uni/2. Semester/Tutorium Info 1"
+conda activate dscb 130
+jupyter lab 
